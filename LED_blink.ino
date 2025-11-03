3c0eb03e735aa1b3755b676ce9753fad1cb99bf2
Megha-Dangi <MEGHADANGI30@gmail.com>
2025-11-03 12:42:08 +0530

    Add LED blink example for Arduino
    added a verified working Arduino code for LED clink using pin 13

---
+++ LED_blink.ino
@@ -0,0 +1,17 @@
+// LED Blink Project
+// Verified working on Arduino UNO / Nano / Mega
+
+// The onboard LED is usually connected to pin 13
+const int ledPin = 13;  
+
+void setup() {
+  // Set the LED pin as output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);   // Turn the LED on
+  delay(1000);                  // Wait for 1 second
+  digitalWrite(ledPin, LOW);    // Turn the LED off
+  delay(1000);                  // Wait for 1 second
+}
